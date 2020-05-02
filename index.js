238ec3d980e5553b48fe768f0a58fdacea849369
Leandro Matos <contato@leandromatos.com.br>
2020-05-02 16:51:15 -0300

    Update rules

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    es6: true,
   },
   parserOptions: {
-    ecmaVersion: 8,
+    ecmaVersion: 11,
     parser: "babel-eslint",
   },
   extends: ["prettier", "plugin:prettier/recommended"],
