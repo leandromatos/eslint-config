c3e220dd603238cadbc4d25838411557eecae9d2
Leandro Matos <contato@leandromatos.com.br>
2023-01-11 18:03:03 -0300

    Remove unnecessary dependencies

--- README.md
+++ README.md
@@ -12,16 +12,16 @@ Create a new `.eslintrc.js` file and export an object containing your settings:
 
 ```js
 module.exports = {
-  extends: ['@leandromatos/eslint-config'],
-}
+  extends: ["@leandromatos/eslint-config"],
+};
 ```
 
 If you are working on a project that uses Vue, you also need to add the settings for Vue to the `.eslintrc.js` file:
 
 ```js
 module.exports = {
-  extends: ['@leandromatos/eslint-config', '@leandromatos/eslint-config/vue'],
-}
+  extends: ["@leandromatos/eslint-config", "@leandromatos/eslint-config/vue"],
+};
 ```
 
 ---
--- vue.js
+++ vue.js
@@ -5,34 +5,34 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: "@babel/eslint-parser",
     requireConfigFile: false,
   },
-  extends: ['prettier', 'plugin:prettier/recommended', 'plugin:vue/recommended'],
-  plugins: ['vue'],
+  extends: ["prettier", "plugin:prettier/recommended", "plugin:vue/recommended"],
+  plugins: ["vue"],
   rules: {
-    'vue/attributes-order': 'off',
-    'vue/component-name-in-template-casing': [
-      'error',
-      'kebab-case',
+    "vue/attributes-order": "off",
+    "vue/component-name-in-template-casing": [
+      "error",
+      "kebab-case",
       {
         registeredComponentsOnly: true,
         ignores: [],
       },
     ],
-    'vue/html-indent': ['error', 2],
-    'vue/max-attributes-per-line': 'off',
-    'vue/multi-word-component-names': 'off',
-    'vue/no-v-html': 'off',
-    'vue/html-self-closing': [
-      'error',
+    "vue/html-indent": ["error", 2],
+    "vue/max-attributes-per-line": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/no-v-html": "off",
+    "vue/html-self-closing": [
+      "error",
       {
         html: {
-          void: 'always',
+          void: "always",
         },
       },
     ],
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/singleline-html-element-content-newline": "off",
   },
-}
+};
