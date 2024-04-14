ee9260222b94e99618a1a31b540bfae86a0c6387
Leandro Matos <contato@leandromatos.com.br>
2024-04-14 18:12:45 -0300

    ci: Remove the build step on the deployment workflow and the lib directory from the .gitignore

---
+++ lib/config.d.ts
@@ -0,0 +1,2 @@
+import { Linter } from 'eslint';
+export declare const config: Linter.Config;
---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+import { Linter } from 'eslint';
+/**
+ * This is a custom ESLint configuration. It extends the recommended ESLint configuration and adds some custom rules.
+ *
+ * @author Leandro Matos
+ * @see {@link https://github.com/leandromatos/eslint-config GitHub} for more information.
+ */
+declare const flatConfig: Linter.FlatConfig[];
+export default flatConfig;
