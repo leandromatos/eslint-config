1f54945730408bf8a735d3b88fed4404e8f0b558
Leandro Matos <contato@leandromatos.com.br>
2024-04-21 13:12:05 -0300

    fix: Disable no-unsafe-assigment rule

--- src/config.ts
+++ src/config.ts
@@ -67,7 +67,7 @@ export const config: Linter.Config = {
         },
       },
       rules: {
-        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-call': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
         '@typescript-eslint/no-unsafe-return': 'warn',
