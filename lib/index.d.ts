c8cbe7ae8a3f91b700d26c4dc501e0792f9e6765
Leandro Matos <contato@leandromatos.com.br>
2024-10-05 08:33:41 -0300

    build: Update dependencies

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Linter } from 'eslint';
-/**
- * This is a custom ESLint configuration. It extends the recommended ESLint configuration and adds some custom rules.
- *
- * @author Leandro Matos
- * @see {@link https://github.com/leandromatos/eslint-config GitHub} for more information.
- */
-export declare const config: Linter.Config[];
