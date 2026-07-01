cb899debd4e34949d675d55efc4f44ffa53a8001
Leandro Matos <contato@leandromatos.com.br>
2026-07-01 00:57:30 -0300

    chore: Add commit-msg hook and editor formatting

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,8 +2,8 @@
  * @type {import('lint-staged').Config}
  */
 export default {
-  '*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}': ['eslint --fix'],
-  '*.{json,jsonc,json5}': ['eslint --fix'],
-  '*.{yml,yaml}': ['eslint --fix'],
+  '*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}': ['prettier --write', 'eslint --fix'],
+  '*.{json,jsonc,json5}': ['prettier --write', 'eslint --fix'],
+  '*.{yml,yaml}': ['prettier --write'],
   '*.md': ['prettier --write', 'eslint --fix'],
 }
